da0ff2c7feb9ad9d72e016b885dc3ad2629edf82
ujjalsingh <singhujjal100@gmail.com>
2020-02-07 19:56:25 -0500

    Basic SignUP page

---
+++ app/src/main/java/com/example/tourguideapp/SignUpActivity.java
@@ -0,0 +1,32 @@
+package com.example.tourguideapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignUpActivity extends AppCompatActivity {
+TextView FirstNameSignup,LastNameSignup,EmailSingup,PhoneSignup;
+Button Signupbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Signupbtn=findViewById(R.id.btnSignUp);
+        FirstNameSignup=findViewById(R.id.firstname);
+        LastNameSignup=findViewById(R.id.lastname);
+        EmailSingup=findViewById(R.id.textViewemail);
+        PhoneSignup=findViewById(R.id.textViewphone);
+
+
+        Signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
