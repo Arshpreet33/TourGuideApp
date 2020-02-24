26041f63737b08867663934cc7b006061513d9de
ujjalsingh <singhujjal100@gmail.com>
2020-02-24 11:16:18 -0500

    user created in signup  attached to firbase

--- app/src/main/java/com/example/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/tourguideapp/MainActivity.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnLogin, btnSignUp;
     TextView txtUserName, txtPassword;
     Intent intent;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnLogin = findViewById(R.id.btnLogin);
         txtUserName = findViewById(R.id.txtUserName);
         txtPassword = findViewById(R.id.txtPassword);
+        fAuth=FirebaseAuth.getInstance();
 
         btnLogin.setOnClickListener(this);
         btnSignUp.setOnClickListener(this);
@@ -31,20 +38,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+
+        String email = txtUserName.getText().toString().trim();
+        String password = txtPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            txtUserName.setError("Email is Required.");
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            txtPassword.setError("Password is Required.");
+            return;
+        }
+
+        if(password.length() < 6){
+            txtPassword.setError("Password Must be >= 6 Characters");
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+
+
         switch (v.getId()) {
             case R.id.btnLogin:
-
                 intent = new Intent(MainActivity.this, HomeActivity.class);
                 break;
-
             case R.id.btnSignUp:
-
-                /**TODO - Intent for SignUpPage
-
                 intent = new Intent(MainActivity.this, SignUpActivity.class);
-                 */
                 break;
-
             default:
                 Toast.makeText(this, "Invalid click operation!", Toast.LENGTH_SHORT).show();
                 break;
--- app/src/main/java/com/example/tourguideapp/SignUpActivity.java
+++ app/src/main/java/com/example/tourguideapp/SignUpActivity.java
@@ -1,36 +1,120 @@
 package com.example.tourguideapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
-TextView FirstNameSignup,LastNameSignup,EmailSingup,PhoneSignup,radioview;
-Button Signupbtn;
-FireBaseAuth Fauth;
-ProgressBar progressBar;
+    TextView FirstNameSignup, LastNameSignup, Password, EmailSingup, PhoneSignup, radioview;
+    Button Signupbtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        Signupbtn=findViewById(R.id.btnSignUp);
-        FirstNameSignup=findViewById(R.id.firstname);
-        LastNameSignup=findViewById(R.id.lastname);
-        EmailSingup=findViewById(R.id.textViewemail);
-        PhoneSignup=findViewById(R.id.textViewphone);
+        Signupbtn = findViewById(R.id.btnSignUp);
+        FirstNameSignup = findViewById(R.id.firstname);
+        LastNameSignup = findViewById(R.id.lastname);
+        Password = findViewById(R.id.passwordsignup);
+        EmailSingup = findViewById(R.id.textViewemail);
+        PhoneSignup = findViewById(R.id.textViewphone);
+        fStore = FirebaseFirestore.getInstance();
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            finish();
+        }
 
 
         Signupbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                final String email = FirstNameSignup.getText().toString().trim();
+                String password = LastNameSignup.getText().toString().trim();
+                final String fullName = EmailSingup.getText().toString();
+                final String phone = PhoneSignup.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    FirstNameSignup.setError("Email is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Password.setError("Password is Required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Password.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+
+                // register the user in firebase
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            //  userID = fAuth.getCurrentUser().getUid();
+                               /* DocumentReference documentReference = fStore.collection("users").document(userID);
+                                Map<String,Object> user = new HashMap<>();
+                                user.put("fName",fullName);
+                                user.put("email",email);
+                                user.put("phone",phone);
+                                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "onSuccess: user Profile is created for "+ userID);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG, "onFailure: " + e.toString());
+                                    }
+                                });*/
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
             }
         });
-
     }
 }
